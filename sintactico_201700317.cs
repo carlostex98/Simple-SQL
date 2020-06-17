d8e98ec2909d06a8f3696f087998d587d67cf8e0
Carlos Alejandro Tenes Mejia <cctenes@gmail.com>
2020-06-16 22:07:23 -0600

    Update

--- sintactico_201700317.cs
+++ sintactico_201700317.cs
@@ -24,8 +24,35 @@ namespace proyecto1
             //this correspond to the base lang start
             if (is_same("crear"))
             {
+                next_t();
+                //ID ( ID TYPE ++ );
 
             }
+            else if (is_same("insertar"))
+            {
+                next_t();
+                //EN ID VALORES (VAL_ARR --rr);
+            }
+            else if (is_same("eliminar"))
+            {
+                next_t();
+                //DE ID DONDE LOGIC_EXPR --rr;
+            }
+            else if (is_same("actualizar"))
+            {
+                next_t();
+            }
+            else if (is_same("seleccionar"))
+            {
+                next_t();
+                //* DE VAL_ARR DONDE LOGIC_XPR;
+            }
+            else
+            {
+                //error
+                //PANIC MODE
+            }
+
         }
 
 
